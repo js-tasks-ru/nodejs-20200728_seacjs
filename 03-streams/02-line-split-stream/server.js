11d5bd8ac17f93c52b483d23bd3fcc97c81b2116
sea-cjs@yandex.ru <sea-cjs@yandex.ru>
2020-08-20 09:42:59 +0300

    in progress

--- 03-streams/02-line-split-stream/server.js
+++ 03-streams/02-line-split-stream/server.js
@@ -1,3 +1,18 @@
-/**
- * Created by seacjs on 19/08/2020.
- */
+const LineSplitStream = require('./LineSplitStream');
+const os = require('os');
+
+const lines = new LineSplitStream({
+    encoding: 'utf-8',
+    objectMode: false,
+    highWaterMark: 2
+});
+
+function onData(line) {
+    console.log(line);
+}
+
+lines.on('data', onData);
+
+lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка`);
+
+lines.end();
