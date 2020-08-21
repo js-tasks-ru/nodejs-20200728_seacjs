66320d77bed1141ec70a168dde48e25ba5d31034
sea-cjs@yandex.ru <sea-cjs@yandex.ru>
2020-08-21 14:44:20 +0300

    decision 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,20 +4,30 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
-    this.currentResult = '';
-    this.nextResult = '';
-    this.separator = 'о'; // os.EOL;
+    this.encoding = options.encoding;
+    this.separator = os.EOL;
+    this.lastString = ''; // помещаем сюда сроку если она не закончилась переносом
   }
 
-    // npm run test:local 03-streams 02-line-split-stream
-    _transform(chunk, encoding, callback) {
-      console.log(chunk);
+  // npm run test:local 03-streams 02-line-split-stream
+  _transform(chunk, encoding, callback) {
+
+    let arrayChank = chunk.toString(this.encoding).split(this.separator);
+    if(arrayChank.length > 0) {
+      for (let i = 0; i < arrayChank.length - 1; i++) {
+        this.push(this.lastString + arrayChank[i]);
+        this.lastString = '';
+      }
+      this.lastString += arrayChank[arrayChank.length - 1];
+    }
+
+    callback();
 
   }
 
-  // _flush(callback) {
-  //   callback(null, this.currentResult);
-  // }
+  _flush(callback) {
+    callback(null, this.lastString);
+  }
 }
 
 module.exports = LineSplitStream;
